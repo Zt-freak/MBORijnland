28a2e02ed7719392b47b564eaba1f77d311347f1
vQFire <youpvleeuwen@gmail.cmo>
2019-09-26 15:08:29 +0200

    LOLOLOLOL

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,8 +2,6 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -23,16 +21,6 @@ class Category
      */
     private $Name;
 
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Product", mappedBy="Category")
-     */
-    private $Products;
-
-    public function __construct()
-    {
-        $this->Products = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -46,37 +34,6 @@ class Category
     public function setName(?string $Name): self
     {
         $this->Name = $Name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Product[]
-     */
-    public function getProducts(): Collection
-    {
-        return $this->Products;
-    }
-
-    public function addProduct(Product $product): self
-    {
-        if (!$this->Products->contains($product)) {
-            $this->Products[] = $product;
-            $product->setCategory($this);
-        }
-
-        return $this;
-    }
-
-    public function removeProduct(Product $product): self
-    {
-        if ($this->Products->contains($product)) {
-            $this->Products->removeElement($product);
-            // set the owning side to null (unless already changed)
-            if ($product->getCategory() === $this) {
-                $product->setCategory(null);
-            }
-        }
 
         return $this;
     }
